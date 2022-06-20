438e6293c87205ca63156a9bb56fea8c3a14b06d
Boğaçhan Yiğitbaşı <bogachanyigitbasi@Bogachans-MacBook-Air.local>
2022-06-20 21:35:29 +0300

    frontend commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+// import logo from './logo.svg';
+// import './App.css';
+import { DAppProvider, ChainId, Kovan } from '@usedapp/core';
+import {Header} from './components/Header'
+import {Container} from "@material-ui/core"
+import {Main} from "./components/Main"
 function App() {
+  
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    <DAppProvider config={{
+      // supportedChains: [ChainId.Kovan, ChainId.Rinkeby, 1337 ],
+      networks:[Kovan],
+      notifications: {
+        expirationPeriod: 1000,
+        checkInterval: 1000
+      }
+    }}>
+      <Header/>
+      <Container maxWidth="md">
+      <Main/>
+      </Container>
+    </DAppProvider>
+  )
+  
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //        Hello!
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ src/hooks/index.ts
@@ -0,0 +1,5 @@
+export {useStakeTokens} from "../hooks/useStakeTokens"
+export {useBuyTokens} from "../hooks/useBuyTokens"
+export {useGasTokensPayments} from "../hooks/useGasTokensPayments"
+// export {useGasPrice} from "../hooks/useGasPrice"
+
